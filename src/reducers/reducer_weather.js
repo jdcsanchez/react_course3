5601522fb9443a137fe0ed0b097d74fcb84e93c2
jdiego82 <jdiego82@yahoo.com>
2017-09-30 00:49:42 +0800

    Finished Section 5, Lecture 61

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,16 @@
-export default function(state = null, action){
-  console.log("Action received ", action);
+import {FETCH_WEATHER} from '../actions/index';
+
+export default function(state = [], action){
+  switch(action.type){
+    case FETCH_WEATHER:
+      //concat returns a new instance of an array with the concatenated value
+      //state.concat([action.payload.data]);
+
+      //ES6 syntax
+      //new array with action.payload.data and all of the entries in state array
+      return [ action.payload.data, ...state]; //[city, city, city, ...]
+    default:
+      break;
+  }
   return state;
 }
